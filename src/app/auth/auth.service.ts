9a1028db5850b317f9f9af5bf80c7e33895a3067
mimsim <mimsim2000@gmail.com>
2021-08-22 12:12:05 +0300

    handle Auth date

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { AuthResponseModel } from '../shared/authResponseModel.model';
+import { UserModel } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<UserModel>();
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +20,14 @@ export class AuthService {
       returnSecureToken: true
     }
     ).pipe(
-      catchError(this.handleError)
+      catchError(this.handleError), tap(resData => {
+       this.handleAuth(
+         resData.email, 
+         resData.localId, 
+         resData.idToken, 
+         +resData.expiresIn
+        );
+      })
     );
   }
   login(email: string, password: string) {
@@ -31,6 +40,13 @@ export class AuthService {
         catchError(this.handleError)
       );
   }
+
+  private handleAuth(email: string, userId: string, token: string, expiresIn: number){
+    const experitionDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new UserModel(email, userId, token, experitionDate);
+    this.user.next(user);
+  }
+
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = 'Unknown error';
     if (!errorRes.error || !errorRes.error.error) {
